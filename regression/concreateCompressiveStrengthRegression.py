e8f07475484fffbb6fc2a145d3c7ea4f013ee43a
Farzan <farzanroxz123@gmail.com>
2022-07-06 15:22:33 +0530

    Linear regression completed

--- regression/concreateCompressiveStrengthRegression.py
+++ regression/concreateCompressiveStrengthRegression.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+#Gathering data
+
+location = "D:\\UCSC\\3rd year\\1st sem\\1. MLNC\\2. Assigment\\19001444\\data\\regression\\Concrete_Data.xls"
+data = pd.read_excel( location )
+
+#Data preprocessing is not required as all the attributes have real/integer values
+
+x = data.iloc[ : , : 8 ]
+y = data.iloc[ : , 8 ]
 
 
-print("hi")
+#seperating train and test data
+
+from sklearn.model_selection import train_test_split
+
+xTrain , xTest , yTrain , yTest = train_test_split( x , y )
+
+from sklearn.linear_model import LinearRegression
+
+dataModel = LinearRegression().fit( xTrain , yTrain )
+
+#Making prediction for test data set
+predictedResults = dataModel.predict( xTest )
+
+#Testing accuracy
+from sklearn.metrics import mean_absolute_error
+meanAbsoluteError = mean_absolute_error( yTest , predictedResults )
+print( meanAbsoluteError )
